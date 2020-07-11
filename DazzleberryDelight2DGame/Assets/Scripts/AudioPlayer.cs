cc1b80b2dedf5206971a29ee4fc3f53ae13d697f
jefftemple82 <61958944+jefftemple82@users.noreply.github.com>
2020-07-11 10:28:48 +0800

    build with start menu and options

---
+++ DazzleberryDelight2DGame/Assets/Scripts/AudioPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DBD.Core
+{
+    public class AudioPlayer : MonoBehaviour
+    {
+        AudioSource audioSource;
+        [SerializeField] AudioSource bgMusic01;
+        OptionsController optionsController;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            audioSource = GetComponent<AudioSource>();
+            audioSource.volume = 0.6f;
+        }
+
+        public void SetVolume(float volume)
+        {
+            audioSource.volume = volume;
+        }
+
+    }
+}
---
+++ DazzleberryDelight2DGame/Assets/Scripts/UIManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DBD.Core
+{
+    public class UIManager : MonoBehaviour
+    {
+        [SerializeField] GameObject primaryMenuCanvas;
+        [SerializeField] GameObject optionsMenuCanvas;
+        [SerializeField] GameObject creditsMenuCanvas;
+
+        OptionsController optionsController;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            optionsController = FindObjectOfType<OptionsController>();
+
+            CloseMenu();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void CloseMenu()
+        {
+            optionsController.SaveOptions();
+
+            primaryMenuCanvas.SetActive(false);
+            optionsMenuCanvas.SetActive(false);        
+            creditsMenuCanvas.SetActive(false);
+        }
+
+        public void OpenOptionsMenu()
+        {
+            primaryMenuCanvas.SetActive(true);
+            optionsMenuCanvas.SetActive(true);
+        }
+
+        public void OpenCreditsMenu()
+        {
+            primaryMenuCanvas.SetActive(true);
+            creditsMenuCanvas.SetActive(true);
+        }
+    }
+}
